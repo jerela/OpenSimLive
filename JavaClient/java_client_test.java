d9c6da04289a157400a5f141edeaa2c220bdaf95
Jere Lavikainen <61044352+jerela@users.noreply.github.com>
2020-08-07 10:35:47 +0300

    Updated java client
    Client now read IP, port etc from conf.txt

--- JavaClient/java_client_test.java
+++ JavaClient/java_client_test.java
@@ -4,6 +4,8 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList; // resizable array
 
 public class java_client_test 
 {
@@ -13,8 +15,38 @@ public class java_client_test
    {
 	int port = 5010;
 	int dataport = -1;
+        String ip = "127.0.0.1";
 	int rev = 1;
-        Client myClient = new Client(port, dataport, "127.0.0.1", rev);
+        
+        ArrayList<String> confContents = new ArrayList<String>();
+        try {
+            File myObj = new File("conf.txt");
+            Scanner textFileReader = new Scanner(myObj);
+            while (textFileReader.hasNextLine()) {
+                String data = textFileReader.nextLine();
+                confContents.add(data);
+            }
+            textFileReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
+        if (confContents.size() > 0)
+            port = Integer.parseInt(confContents.get(0));
+        if (confContents.size() > 1)
+            dataport = Integer.parseInt(confContents.get(1));
+        if (confContents.size() > 2)
+            ip = confContents.get(2);
+        if (confContents.size() > 3)
+            rev = Integer.parseInt(confContents.get(3));
+        
+        System.out.println("Port: " + port);
+        System.out.println("Dataport: " + dataport);
+        System.out.println("IP: " + ip);
+        System.out.println("Reverse bytes: " + rev);
+        
+        Client myClient = new Client(port, dataport, ip, rev);
         double C[];
         C = new double[6];
         
