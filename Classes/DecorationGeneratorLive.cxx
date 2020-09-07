a02bf03ab19e8822f6a81e9c842ede01f248e8a7
Jere Lavikainen <61044352+jerela@users.noreply.github.com>
2020-09-07 15:15:12 +0300

    Update DecorationGeneratorLive.cxx

--- Classes/DecorationGeneratorLive.cxx
+++ Classes/DecorationGeneratorLive.cxx
@@ -9,7 +9,7 @@ DecorationGeneratorLive::~DecorationGeneratorLive() {}
 
 void DecorationGeneratorLive::generateDecorations(const SimTK::State& state, SimTK::Array_<SimTK::DecorativeGeometry>& geometry) {
 	// create an array to hold the decorative sphere
-	SimTK::Array_<SimTK::DecorativeGeometry> geomArray(2);
+	SimTK::Array_<SimTK::DecorativeGeometry> geomArray(3);
 	// create the decorative sphere
 	SimTK::DecorativeSphere sphere(0.02);
 	// make the sphere blue
@@ -21,18 +21,27 @@ void DecorationGeneratorLive::generateDecorations(const SimTK::State& state, Sim
 	// set the sphere's location in its coordinate system
 	sphere.setTransform(transform_);
 	// put the sphere into an array
-	geomArray[0] = sphere;
-
+	
 	// let's also create an arrow from the pelvis, pointing like the mirrored rotation of the body
-	SimTK::DecorativeArrow arrow(SimTK::Vec3(0, 0, 0), SimTK::Vec3(1, 0, 0), 100);
+	SimTK::DecorativeLine arrow(SimTK::Vec3(0, 0, 0), SimTK::Vec3(1, 0, 0));
 	arrow.setColor({ 0,0,1 });
 	arrow.setBodyId(referenceBodyId_);
-	arrow.setStartPoint(SimTK::Vec3(0, 0, 0));
-	arrow.setStartPoint(SimTK::Vec3(1, 1, 1));
+	arrow.setPoint1(SimTK::Vec3(0, 0, 0));
+	arrow.setPoint2(SimTK::Vec3(1, 1, 1));
 	arrow.setOpacity(0.5);
 	arrow.setLineThickness(10);
 	arrow.setTransform(arrowDir_);
-	geomArray[1] = arrow;
+	geomArray[0] = arrow;
+
+	SimTK::DecorativeCylinder cylinder(0.5, 0.5);
+	cylinder.setBodyId(referenceBodyId_);
+	cylinder.setOpacity(0.5);
+	cylinder.setColor({ 0,0,1 });
+	cylinder.setTransform(arrowDir_);
+	geomArray[1] = cylinder;
+
+	geomArray[2] = sphere;
+
 
 	// save the array as the resulting geometry to be shown
 	geometry = geomArray;
